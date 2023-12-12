20079fd3a5f106ab7e81ef4f6c0a41c548a09270
RF&CTI <farleyK@protonmail.com>
2023-12-12 17:45:27 +0800

    4

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 const API_BASE_URL = 'https://virusshare.com/apiv2';
 
-export async function getVirusInfo(apiKey: string, hash: string): Promise<any> {
+export async function getVirusInfo(hash: string): Promise<any> {
   try {
-    const response = await axios.get(`${API_BASE_URL}/virus-info?apikey=${apiKey}&hash=${hash}`);
+    const response = await axios.get(`${API_BASE_URL}/virus-info?apikey=${process.env.VIRUS_API_KEY}&hash=${hash}`);
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch virus information');
   }
 }
 
-export async function searchViruses(apiKey: string, query: string): Promise<any> {
+export async function searchViruses(query: string): Promise<any> {
   try {
-    const response = await axios.get(`${API_BASE_URL}/search-viruses?apikey=${apiKey}&query=${query}`);
+    const response = await axios.get(`${API_BASE_URL}/search-viruses?apikey=${process.env.VIRUS_API_KEY}&query=${query}`);
     return response.data;
   } catch (error) {
     throw new Error('Failed to search viruses');
