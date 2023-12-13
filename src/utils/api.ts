46cbda9b627b3a25ed1b72f9836877f39b31f8a0
RF&CTI <112944137+RFCTI@users.noreply.github.com>
2023-12-13 09:38:44 +0800

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,26 +1,43 @@
-import axios from 'axios';
-
 const API_BASE_URL = 'https://virusshare.com/apiv2';
+const API_KEY = process.env.VIRUS_SHARE_API_KEY; // 从环境变量获取Virus share API的Key
 
-export async function getVirusInfo(hash: string): Promise<any> {
+/**
+ * 延迟执行的Promise函数
+ * @param {number} ms 毫秒数
+ * @returns Promise
+ */
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * 查询哈希值
+ * @param {string} hashString MD5或哈希字符串
+ * @returns Promise
+ */
+async function queryHash(hashString: string): Promise<any> {
+  // 构建请求URL
+  const url = `${API_BASE_URL}/<request>?apikey=${API_KEY}&hash=${hashString}`;
+
   try {
-    const response = await axios.get(`${API_BASE_URL}/virus-info?apikey=${process.env.VIRUS_API_KEY}&hash=${hash}`);
-    return response.data;
+    // 发送请求前等待25秒
+    await delay(25000);
+
+    // 发送请求
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    // 检查响应状态
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // 解析JSON响应
+    const data = await response.json();
+    return data;
   } catch (error) {
-    throw new Error('Failed to fetch virus information');
+    console.error('Error querying hash:', error);
+    throw error;
   }
 }
 
-export async function searchViruses(query: string): Promise<any> {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/search-viruses?apikey=${process.env.VIRUS_API_KEY}&query=${query}`);
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to search viruses');
-  }
-}
-
-export default {
-  getVirusInfo,
-  searchViruses,
-};
+export { queryHash };
